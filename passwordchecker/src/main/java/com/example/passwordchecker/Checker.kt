3eafa14831266c00d490574e39918df740b6f7bf
jamesrhdoes <ankur@agpaytech.co.uk>
2021-08-06 18:55:42 +0530

    add isvalied function

--- passwordchecker/src/main/java/com/example/passwordchecker/Checker.kt
+++ passwordchecker/src/main/java/com/example/passwordchecker/Checker.kt
@@ -7,14 +7,14 @@ class Checker(
     private lateinit var current: String
     private lateinit var previous: String
 
-    private val CAPITAL_LETTER="[ A-Za-z] "
+    private val CAPITAL_LETTER = "[ A-Za-z] "
 
     init {
         if (typedPassword.isNotBlank()
             && typedPassword.isNotEmpty()
             && currentPassword.isNotEmpty()
             && currentPassword.isNotBlank()
-        ){
+        ) {
             current = currentPassword
             previous = typedPassword
         }
@@ -22,6 +22,9 @@ class Checker(
 
     fun isCorrect(): Boolean {
         if (current.length != previous.length) {
+            return false
+        }
+        if(!isValied()){
             return false
         }
         return typedPassword == currentPassword
@@ -34,11 +37,23 @@ class Checker(
     // a number and
     // a special character.
 
-    fun isValied():Boolean{
-        if(current.length<6){
+    fun isValied(): Boolean {
+        if (current.length < 6) {
             return false
         }
 
+        var count = 0;
+        for (letter in current) {
+            if ((letter >= 'a' && letter <= 'z')
+                || (letter >= 'A' && letter <= 'Z')
+                || (letter >= '0' && letter <= '9')
+                || (letter >= '!' && letter <= '&')
+            ) {
+                count += 1
+            }
+        }
+
+        return count==4
     }
 
 }
